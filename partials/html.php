dece8f78d4d8f920c0ae3536cf841c03d47714dc
Victor Rafael de Oliveira Silva <victorafaeloliveira@gmail.com>
2024-04-04 13:03:32 -0300

    git

--- partials/html.php
+++ partials/html.php
@@ -1,4 +1,4 @@
 <!DOCTYPE html>
-<html lang="pt-BR" data-layout-mode="detached" data-topbar-color="dark" data-menu-color="dark" data-sidenav-user="true">
+<html lang="pt-BR" data-layout-mode="fluid" data-topbar-color="dark" data-menu-color="dark" data-sidenav-user="true">
 
 <?php include ('./conexao/database.php') ?>

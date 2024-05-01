be41f15d3c8b155c37ada41c217c06e110d2b038
Victor Rafael de Oliveira Silva <victorafaeloliveira@gmail.com>
2024-05-01 13:23:19 -0300

    ajuste de index

--- index.php
+++ index.php
@@ -129,7 +129,7 @@
         <!-- end auth-fluid-form-box-->
 
         <!-- Auth fluid right content -->
-        <div class="auth-fluid-right text-center">
+        <div class="auth-fluid-right text-center d-none">
             <div class="auth-user-testimonial">
                 <h2 class="mb-3">I love the color!</h2>
                 <p class="lead"><i class="mdi mdi-format-quote-open"></i> It's a elegent templete. I love it very much! . <i class="mdi mdi-format-quote-close"></i>
